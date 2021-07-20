81b7ab50b0c5170cc6cdab49534576394dea644f
mnawrot <mateusz.nawrot@westwing.pl>
2021-07-20 08:52:47 +0200

    update VM test

--- app/src/test/java/de/westwing/campaignbrowser/CampaignViewModelTest.kt
+++ app/src/test/java/de/westwing/campaignbrowser/CampaignViewModelTest.kt
@@ -1,10 +1,12 @@
 package de.westwing.campaignbrowser
 
+import org.junit.Assert.fail
 import org.junit.Test
 
 class CampaignViewModelTest {
 
     @Test
     fun `Test CampaignViewModel here`() {
+        fail()
     }
 }
